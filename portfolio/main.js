a4fd13ebc441705084ef08333433da069dc048ea
HarikaBontha <162826965+HarikaBontha@users.noreply.github.com>
2024-07-15 19:34:08 +0530

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,8 @@
+/* ----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText",{
+    strings : ["Harika"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
